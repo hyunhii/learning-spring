c503d43e59567649fb4e90355cc33ce5a783a51b
hyunhii <be_hyunny@naver.com>
2023-09-21 17:13:32 +0900

    feat:섹션 6. 컴포넌트 스캔 - 컴포넌트 스캔과 의존관계 자동 주입 시작

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     //DIP 위반
@@ -8,6 +12,7 @@ public class MemberServiceImpl implements MemberService{
     // DIP 만족
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
