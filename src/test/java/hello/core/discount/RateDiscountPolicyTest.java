8334c3717eff3ea8b9c4054fb046939dd904c027
hyunhii <be_hyunny@naver.com>
2023-09-22 16:34:03 +0900

    feat:섹션 7. 의존관계 자동 주입 - 옵션 처리

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -36,7 +36,7 @@ class RateDiscountPolicyTest {
         int discount = rateDiscountPolicy.discount(member, 10000);
 
         //then
-        Assertions.assertThat(discount).isEqualTo(1000);
+        Assertions.assertThat(discount).isEqualTo(0);
     }
 
 }
